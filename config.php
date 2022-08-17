af0e8a7eb4dbe1e691a2759e3aad6f241a4cdd16
shadiwantoro <shadiwantoro@gmail.com>
2022-08-18 04:06:08 +0700

    First Upload

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+#Config file
+//Site Title
+$title = "JSPlayer - Free Iptv Player";
+
+//Site URL
+$site_url = "";
+
+//Site Description
+$description = "";
+
+//Watermark image
+$watermarkImage = "";
+
+//Watermark link
+$watermarkLink = "";
+
+//Google Analytics tag ID
+$gaCode = "UA-69771681-1";
+
+//Preroll Ads Video (.mp4)
+$prerollAds = "https://dl3.3rver.org/cdn2/02/film/2019/The%20Avengers%3A%20Endgame/Avengers.Endgame.2019.Trailer.mp4";
+
+//Preroll Ads Time (seconds)
+$prerollTime = 5;
+
+//Preroll Ads External Link
+$prerollLink = "http://www.mokoshalb.com/buy-edu-mail/";
+?>
